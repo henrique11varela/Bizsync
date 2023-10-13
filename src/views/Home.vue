9687d8f73b87c5796fe500b2f7f685aa5cfc9391
Deadfaced <btteixeira.bt@gmail.com>
2023-10-13 21:03:32 +0100

    added values folder to assets and added values to the home web page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,44 @@
 
         <div class="values">
             <div class="valueTitle">
-                <h3>OS NOSSOS VALORES</h3>
+                <h1>OS NOSSOS VALORES</h1>
             </div>
             <div class="valueContents">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente eius saepe accusantium, a cupiditate est molestias, tempora illo consequuntur molestiae aperiam nesciunt error dignissimos atque! Rem aspernatur incidunt quia! Eligendi!
+                <div class="imgTxt">
+                    <img src="../assets/values/excelencia.jpg" alt="excelencia">
+                    <div class="excel">
+                        <h3>Excelência</h3>
+                        <p>Procuramos a excelência em todas as nossas ações, desde a seleção do produto até ao atendimento ao cliente.</p>
+                    </div>
+                </div>
+                <div class="imgTxt">
+                    <div class="transp">
+                        <h3>Transparência</h3>
+                        <p>Operamos de forma aberta e honesta, fornecendo informações claras e precisas aos nossos clientes.</p>
+                    </div>
+                    <img src="../assets/values/transparencia.jpg" alt="transparencia">
+                </div>
+                <div class="imgTxt">
+                    <img src="../assets/values/integridade.jpg" alt="integridade">
+                    <div class="integr">
+                        <h3>Integridade</h3>
+                        <p>Agimos com integridade em todas as nossas interações, mantendo os mais elevados padrões éticos.</p>
+                    </div>
+                </div>
+                <div class="imgTxt">
+                    <div class="cresc">
+                        <h3>Crescimento Sustentável</h3>
+                        <p>Procuramos um crescimento sustentável que beneficie os nossos clientes, equipa e comunidade.</p>
+                    </div>
+                    <img src="../assets/values/crescimento sustentável.jpg" alt="crescimentoSustentavel">
+                </div>
+                <div class="imgTxt">
+                    <img src="../assets/values/confianca.jpg" alt="confianca">
+                    <div class="conf">
+                        <h3>Confiança</h3>
+                        <p>Baseamos todas as nossas relações comerciais na confiança mútua, agindo com honestidade e cumprindo as nossas promessas. </p>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -36,7 +70,7 @@
 </script>
 
 <style scoped>
-h3{
+h1, h3{
     color: #008ecc;
 }
 
@@ -49,12 +83,16 @@ h3{
 }
 .values{
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: space-evenly;
     align-items: center;
     width: 80%;
-    height: 7rem;
+    height: max-content;
     border-bottom: solid 1px black;
     padding: 2rem 0;
+}
+.valueTitle{
+    margin-bottom: 3rem;
 }
 
 .partners{
@@ -81,4 +119,29 @@ h3{
     width: 270px;
     height: 180px;
 }
+
+.excel, .transp, .integr, .cresc, .conf{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    padding: 0 10rem;
+}
+
+.valueContents{
+    overflow: hidden;
+}
+.valueContents img{
+    width: 270px;
+    height: 100%;
+}
+
+.imgTxt{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20%;
+}
+
 </style>
